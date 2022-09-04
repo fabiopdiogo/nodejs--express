8ffc83e4b229671d980a486c5b212b436f326168
Fabio Diogo <fabindiogo@gmail.com>
2022-09-04 11:27:02 -0300

    Criado login e autenticação com jwt

--- src/modules/user/userController.js
+++ src/modules/user/userController.js
@@ -1,12 +1,15 @@
 import {Router} from 'express'
 
-import { signup } from './userService'
+import { signup, login } from './userService'
+
+const AUTH_COOKIE_NAME = 'authorization'
 
 const router = Router()
+
 router.post('/signup', (req, res) =>{
   try{    
-    const answer = signup(req.body)
-    res.send(answer) 
+    const token = signup(req.body)
+    res.cookie(AUTH_COOKIE_NAME, token).status(200).send() 
   }catch (err){
    if (err.message === 'email_existente')
     return res.status(400).send(err.message)
@@ -15,9 +18,16 @@ router.post('/signup', (req, res) =>{
   }
 })
 
-router.post('/login', (req, res) =>{
-  res.send('LOGIN /') 
-})
+router.post('/login', (req, res) =>{  
+  try{
+    const token = login(req.body)
+    res.cookie(AUTH_COOKIE_NAME, token).status(200).send()
+  } catch (err){
+    if (err.message === 'email_nao_encontrado' || err.message === 'senha_incorreta')
+      return res.status(400).send(err.message)
 
+      res.status(500).send()
+  }
+})
 
 export default router
